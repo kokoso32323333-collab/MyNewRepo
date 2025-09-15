07b496b3f92e3fcdf8b573160fe4a0286325ee38
kokoso32323333-collab <kokoso32323333@gmail.com>
2025-09-15 15:35:33 +0900

    update file

--- BinarySearch.py
+++ BinarySearch.py
@@ -8,7 +8,7 @@ while left <= right:
     middle = (left + right) // 2
   
     if a[middle] == int(x):  # Success
-        print('There is {:3} at a[{:2}] in the list.'.format(x, middle))
+        print('There ttt is {:3} at a[{:2}] in the list.'.format(x, middle))
         break   
     elif a[middle] < int(x):
         left = middle + 1    
